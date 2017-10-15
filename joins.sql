570686e4f09db335f18200727c3726bfdddd9ba1
Virginia Souza <virginiasouza@Virginias-MacBook-Pro.local>
2017-10-14 15:23:08 -1000

    completed queries

--- joins.sql
+++ joins.sql
@@ -1,44 +1,77 @@
--- Create a query to get all fields from the users table
+-- -- Create a query to get all fields from the users table
 SELECT *
 FROM users;
 
--- Create a query to get all fields from the posts table where the user_id is 100
+-- -- Create a query to get all fields from the posts table where the user_id is 100
 SELECT *
 FROM posts
 WHERE users_id = 100;
 
--- Create a query to get all posts fields, the user's first name, and the user's last name, from the posts table where the user's id is 200
+-- -- Create a query to get all posts fields, the user's first name, and the user's last name, from the posts table where the user's id is 200
 SELECT posts.*, first_name, last_name
 FROM posts, users
 WHERE users_id = 200;
 
--- Create a query to get all posts fields, and the user's username, from the posts table where the user's first name is 'Norene' and the user's last_name is 'Schmitt'
+-- -- Create a query to get all posts fields, and the user's username, from the posts table where the user's first name is 'Norene' and the user's last_name is 'Schmitt'
 SELECT posts.*, username
 FROM posts, users
 WHERE users.first_name = 'Norene' AND users.last_name = 'Schmitt';
 
--- Create a query to get usernames from the users table where the user has created a post after January 1, 2015 (26514 rows)
+-- -- Create a query to get usernames from the users table where the user has created a post after January 1, 2015 (26514 rows)
 SELECT username
 FROM users
 WHERE created_at >= '2015-01-01 00:00:00.000';
 
 -- Create a query to get the post title, post content, and user's username where the user who created the post joined before January 1, 2015
 SELECT posts.title, posts.content, users.username
-FROM posts, users
+FROM posts INNER JOIN users ON (posts.users_id = users.id)
 WHERE users.created_at <= '2015-01-01 00:00:00.000';
 
--- Create a query to get the all rows in the comments table, showing post title (aliased as 'Post Title'), and the all the comment's fields
+-- Create a query to get the all rows in the comments table, showing post title (aliased as 'Post Title'), and the all the comment's fields (19729 rows)
 
+SELECT comments.*, posts.title AS "Post Title"
+FROM comments
+JOIN posts ON (comments.posts_id = posts.id);
 
--- Create a query to get the all rows in the comments table, showing post title (aliased as post_title), post url (ailased as post_url), and the comment body (aliased as comment_body) where the post was created before January 1, 2015
+-- Create a query to get the all rows in the comments table, showing post title (aliased as post_title), post url (ailased as post_url), and the comment body (aliased as comment_body) where the post was created before January 1, 2015 (9126 rows)
 
+SELECT posts.title AS post_title, posts.url AS post_url, comments.body AS comment_body
+FROM comments JOIN posts ON (comments.posts_id = posts.id)
+WHERE posts.created_at <= '2015-01-01 00:00:00.000';
 
 -- Create a query to get the all rows in the comments table, showing post title (aliased as post_title), post url (ailased as post_url), and the comment body (aliased as comment_body) where the post was created after January 1, 2015
 
+SELECT posts.title AS post_title, posts.url AS post_url, comments.body AS comment_body
+FROM comments JOIN posts ON (comments.posts_id = posts.id)
+WHERE posts.created_at >= '2015-01-01 00:00:00.000';
 
--- Create a query to get the all rows in the comments table, showing post title (aliased as post_title), post url (ailased as post_url), and the comment body (aliased as comment_body) where the comment body contains the word 'USB'
+-- Create a query to get the all rows in the comments table, showing post title (aliased as post_title), post url (ailased as post_url), and the comment body (aliased as comment_body) where the comment body contains the word 'USB' (728 rows)
 
+SELECT posts.title AS post_title, posts.url AS post_url, comments.body AS comment_body
+FROM comments JOIN posts ON (comments.posts_id = posts.id)
+WHERE comments.body LIKE '%USB%';
 
 -- Create a query to get the post title (aliased as post_title), first name of the author of the post, last name of the author of the post, and comment body (aliased to comment_body), where the comment body contains the word 'matrix' ( should have 855 results )
+
+SELECT posts.title AS post_title, users.first_name AS first_name, users.last_name AS last_name, comments.body AS comment_body
+FROM posts JOIN users ON (posts.users_id = users.id)
+ JOIN comments ON (comments.posts_id = posts.id)
+WHERE comments.body LIKE '%matrix%';
+
+
 -- Create a query to get the first name of the author of the comment, last name of the author of the comment, and comment body (aliased to comment_body), where the comment body contains the word 'SSL' and the post content contains the word 'dolorum' ( should have 102 results )
+
+SELECT users.first_name AS first_name, users.last_name AS last_name, comments.body AS comment_body
+FROM posts JOIN users ON (posts.users_id = users.id)
+ JOIN comments ON (comments.posts_id = posts.id)
+WHERE comments.body LIKE '%SSL%' AND posts.content LIKE '%dolorum%';
+
+
 -- Create a query to get the first name of the author of the post (aliased to post_author_first_name), last name of the author of the post (aliased to post_author_last_name), the post title (aliased to post_title), username of the author of the comment (aliased to comment_author_username), and comment body (aliased to comment_body), where the comment body contains the word 'SSL' or 'firewall' and the post content contains the word 'nemo' ( should have 218 results )
+
+SELECT users.first_name AS post_author_first_name, users.last_name AS post_author_last_name, posts.title AS post_title, users.username AS comment_author_username, comments.body AS comment_body
+FROM posts JOIN users ON (posts.users_id = users.id)
+  JOIN comments ON (comments.posts_id = posts.id)
+WHERE posts.content LIKE '%nemo%'
+  AND (comments.body LIKE '%SSL%' OR comments.body LIKE '%firewall%');
+
